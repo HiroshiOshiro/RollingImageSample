8f53d0004466b12e0783eccbfdb09ef1d8ddd21c
hiroshi oshiro <hiroshi.oshiro@travelbook.ph>
2018-12-03 23:36:54 +0900

    コメント追加

--- RollingImage/ViewController.swift
+++ RollingImage/ViewController.swift
@@ -14,25 +14,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // 背景のグレー
         let grayView = UIView(frame: self.view.frame)
         grayView.backgroundColor = UIColor.gray
         self.view.addSubview(grayView)
         
+        // 画像
         let image = UIImage(named: "moon") // http://icooon-mono.com/11223-夜空のフリーアイコン/ より
         let imageView = UIImageView(image: image)
         imageView.center = grayView.center
         grayView.addSubview(imageView)
         
+        // アニメーション
         let rollingAnimation = CABasicAnimation(keyPath: "transform.rotation")
         rollingAnimation.fromValue = 0
         rollingAnimation.toValue = CGFloat.pi * 2.0
-        
-        rollingAnimation.duration = 2.0
-        rollingAnimation.repeatDuration = CFTimeInterval.infinity
-        imageView.layer.add(rollingAnimation, forKey: "rotateAnim")
+        rollingAnimation.duration = 2.0 // 周期２秒
+        rollingAnimation.repeatDuration = CFTimeInterval.infinity // 無限に
+        imageView.layer.add(rollingAnimation, forKey: "rotateAnim") // アニメーションを追加
         
     }
-
-
 }
 
